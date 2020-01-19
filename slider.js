cf44201b1032686421c4b80d384cc41ae6edf919
nkkunzler <nkkunzler@gmail.com>
2020-01-19 01:38:10 -0700

    Updated index.html

--- slider.js
+++ slider.js
@@ -1,5 +1,6 @@
 "use strict"
 const LOG_BASE = 2;
+var microphoneEnabled = false
 
 function abbrFreqId(id){
     document.getElementById(id).value = abbrFreq(document.getElementById(id).value);
@@ -24,14 +25,16 @@ function expandFreq(val){
     }
 }
 
-function getVal(id){
-    var val = document.getElementById(id).value;
-    return (Math.pow(2, val)).toFixed(1);
+function getVal(id, base){
+    var val = Number(document.getElementById(id).value);
+    if (base == 0)
+        return val;
+    return (Math.pow(base, val)).toFixed(1);
 }
 
 function setVal(id, val){
-    if (val < 1)
-        val = 1;
+    if (val < 0)
+        val = 0;
     else if (val > 24000)
         val = 24000;
     document.getElementById(id).value = abbrFreq(val);
@@ -40,4 +43,17 @@ function setVal(id, val){
 function removeHz(id) {
     var val = document.getElementById(id).value;
     document.getElementById(id).value = expandFreq(val);
+}
+
+function toggleMicrophone(btn) {
+    microphoneEnabled = !microphoneEnabled;
+    for (var i = 0; i < arguments.length; i++){
+        if (microphoneEnabled){
+            document.getElementById(arguments[i]).disabled = true;
+            document.getElementById(arguments[i]).style.opacity = 0.1;
+        } else {
+            document.getElementById(arguments[i]).disabled = false;
+            document.getElementById(arguments[i]).style.opacity = 1;
+        }
+    }
 }
